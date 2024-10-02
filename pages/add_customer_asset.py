fabbffc79569f282f3ac1472b8e7b6094e16f1da
manika046 <kasulamanika00@gmail.com>
2024-10-02 17:52:11 +0545

    pom

--- pages/add_customer_asset.py
+++ pages/add_customer_asset.py
@@ -12,49 +12,65 @@ class AddCustomerAsset:
   def __init__(self, driver):
     self.driver = driver
     
+    self.add_customer = '//android.widget.TextView[@text="Add Customer Assets"]'
+    self.choose_customer = '//android.widget.TextView[@text="Choose customer"]'
+    self.ver = '//android.widget.TextView[@text="Select a Customer"]'
+    self.select_customer = '//android.view.ViewGroup[@resource-id="default-100 ASSETS CUSTOMER"]/android.view.ViewGroup'
+    self.choose_location = '//android.widget.TextView[@text="Choose location"]'
+    self.sel_ver = '//android.widget.TextView[@text="Select an Item"]'
+    self.select_item = '//android.widget.TextView[@text="100 Assets -customer -shipto"]'
+    self.choose_asset_type = '//android.widget.TextView[@text="Choose asset type"]'
+    self.select_asset_type = '//android.widget.TextView[@text="vehicle"]'
+    self.asset_name = '//android.widget.EditText[@text="Enter asset name"]'
+    self.fuel_capacity = '//android.widget.EditText[@text="Enter capacity"]'
+    self.license_num = '//android.widget.EditText[@text="Enter Licenseplate No."]'
+    self.select_product = '(//android.widget.TextView[@text=""])[4]'
+    self.product = '//android.widget.TextView[@text="101- Package product"]'
+    self.create_button = '//android.widget.TextView[@text="Create"]'
+    
   def AddCustomer(self):
-    add_customer = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Add Customer Assets"]')
+    add_customer = self.driver.find_element(by=AppiumBy.XPATH, value=self.add_customer)
     add_customer.click()
     time.sleep(2)
     
   def ChooseCustomer(self):
-    choose_customer = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Choose customer"]')
+    choose_customer = self.driver.find_element(by=AppiumBy.XPATH, value=self.choose_customer)
     choose_customer.click()
     time.sleep(2)
     
   def CustomerVerify(self):
-    ver = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Select a Customer"]')
+    ver = self.driver.find_element(by=AppiumBy.XPATH, value=self.ver)
     assert ver.text == "Select a Customer"
     print("ANOTHER STAGE!")
     
   def SelectCustomer(self):
-    select_customer = self.driver.find_element(by=AppiumBy.XPATH, value='//android.view.ViewGroup[@resource-id="default-100 ASSETS CUSTOMER"]/android.view.ViewGroup')
+    select_customer = self.driver.find_element(by=AppiumBy.XPATH, value=self.select_customer)
     select_customer.click()
     time.sleep(2)
     
   def ChooseLocation(self):
-    choose_location = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Choose location"]')
+    choose_location = self.driver.find_element(by=AppiumBy.XPATH, value=self.choose_location)
     choose_location.click()
     time.sleep(2)
     
   def SelectVerify(self):
-    sel_ver = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Select an Item"]')
+    sel_ver = self.driver.find_element(by=AppiumBy.XPATH, value=self.sel_ver)
     assert sel_ver.text == "Select an Item"
     print("SELECT AN ITEM! ")
     
   def SelectItem(self):
-    select_item = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="100 Assets -customer -shipto"]')
+    select_item = self.driver.find_element(by=AppiumBy.XPATH, value=self.select_item)
     select_item.click()
     time.sleep(2)
     
   def ChooseAssetType(self):
-    choose_asset_type = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Choose asset type"]')
+    choose_asset_type = self.driver.find_element(by=AppiumBy.XPATH, value=self.choose_asset_type)
     choose_asset_type.click()
     time.sleep(2)
     
   def SelectAssetType(self):
     # for vehicle
-    select_asset_type = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="vehicle"]')
+    select_asset_type = self.driver.find_element(by=AppiumBy.XPATH, value=self.select_asset_type)
     select_asset_type.click()
     time.sleep(2)
     
@@ -63,31 +79,31 @@ class AddCustomerAsset:
     # print("VEHICLE SELECTED SUCCESSFUL! ")
     
   def AssetName(self, asset):
-    asset_name = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.EditText[@text="Enter asset name"]')
+    asset_name = self.driver.find_element(by=AppiumBy.XPATH, value=self.asset_name)
     asset_name.send_keys(asset)
     time.sleep(2)
     
   def FuelCapacity(self, capacity):
-    fuel_capacity = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.EditText[@text="Enter capacity"]')
+    fuel_capacity = self.driver.find_element(by=AppiumBy.XPATH, value=self.fuel_capacity)
     fuel_capacity.send_keys(capacity)
     time.sleep(2)
     
   def LicenseNumber(self):
-    license_num = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.EditText[@text="Enter Licenseplate No."]')
+    license_num = self.driver.find_element(by=AppiumBy.XPATH, value=self.license_num)
     license_num.send_keys(random.choice(PlateNum))
     time.sleep(2)
     
   def SelectProduct(self):
-    select_product = self.driver.find_element(by=AppiumBy.XPATH, value='(//android.widget.TextView[@text=""])[4]')
+    select_product = self.driver.find_element(by=AppiumBy.XPATH, value=self.select_product)
     select_product.click()
     time.sleep(2)
     
-    product = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="101- Package product"]')
+    product = self.driver.find_element(by=AppiumBy.XPATH, value=self.product)
     product.click()
     time.sleep(2)
     
   def CreateButton(self):
-    create_button = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Create"]')
+    create_button = self.driver.find_element(by=AppiumBy.XPATH, value=self.create_button)
     create_button.click()
     time.sleep(2)
     
--- pages/dashboard.py
+++ pages/dashboard.py
@@ -8,58 +8,68 @@ class DashBoard:
     self.driver = driver
     
     self.allow_el = '//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_allow_button"]'
+    self.dont_allow_element = '//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_deny_button"]'
+    self.loc_text = '//android.widget.TextView[@text="Location Access"]'
+    self.loc_allow = '//android.widget.TextView[@text="Allow"]'
+    self.loc_access = '//android.widget.TextView[@resource-id="com.android.permissioncontroller:id/permission_message"]'
+    self.loc_deny = '//android.widget.TextView[@text="Deny"]'
+    self.using_el = '//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_allow_foreground_only_button"]'
+    self.only_el = '//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_allow_one_time_button"]'
+    self.dont_el = '//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_deny_button"]'
+    self.okay_el = '//android.widget.TextView[@text="Great! you do not have any tasks right now to perform"]'
+    self.menu_bar = '//android.widget.TextView[@text=""]'
   
   def AllowElem(self):
     allow_el = self.driver.find_element(by=AppiumBy.XPATH, value=self.allow_el)
     allow_el.click()
 
   def DontAllowElem(self):
-    dont_allow_element = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_deny_button"]')
+    dont_allow_element = self.driver.find_element(by=AppiumBy.XPATH, value=self.dont_allow_element)
     dont_allow_element.click()
     time.sleep(2)
 
   def LocationText(self):
-    loc_text = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Location Access"]')
+    loc_text = self.driver.find_element(by=AppiumBy.XPATH, value=self.loc_text)
     assert loc_text.text == "Location Access"
     print("STAGE CLEAR! ")
 
   def LocationAllow(self):
-    loc_allow = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Allow"]')
+    loc_allow = self.driver.find_element(by=AppiumBy.XPATH, value=self.loc_allow)
     loc_allow.click()
     time.sleep(2)
 
   def LocationAccess(self):
-    loc_access = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@resource-id="com.android.permissioncontroller:id/permission_message"]')
+    loc_access = self.driver.find_element(by=AppiumBy.XPATH, value=self.loc_access)
     assert loc_access.text == "Allow Staging Fleetpanda to access this device’s location?"
     print("STAGE2 CLEAR! ")
 
   def LocationDeny(self):
-    loc_deny = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Deny"]')
+    loc_deny = self.driver.find_element(by=AppiumBy.XPATH, value=self.loc_deny)
     loc_deny.click()
     time.sleep(2)
 
   def UsingApp(self):
-    using_el = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_allow_foreground_only_button"]')
+    using_el = self.driver.find_element(by=AppiumBy.XPATH, value=self.using_el)
     using_el.click()
     time.sleep(2)
   
   def OnlyThisTime(self):
-    only_el = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_allow_one_time_button"]')
+    only_el = self.driver.find_element(by=AppiumBy.XPATH, value=self.only_el)
     only_el.click()
     time.sleep(2)
 
   def DontAllow(self):
-    dont_el = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_deny_button"]')
+    dont_el = self.driver.find_element(by=AppiumBy.XPATH, value=self.dont_el)
     dont_el.click()
     time.sleep(2)
 
   # only when there is no tasks assign
   def OkayElem(self):
-    okay_el = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Great! you do not have any tasks right now to perform"]')
+    okay_el = self.driver.find_element(by=AppiumBy.XPATH, value=self.okay_el)
     assert "Great!" in okay_el.text
     print("OKAY! ")
   
   def MenuBar(self):
-    menu_bar = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text=""]')
+    menu_bar = self.driver.find_element(by=AppiumBy.XPATH, value=self.menu_bar)
     menu_bar.click()
     time.sleep(2)
